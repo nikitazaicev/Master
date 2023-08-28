78997bf14c2851cdfe6756ed330a47cd13912113
nikitazaicev <nikitazaicev@inbox.lv>
2023-08-28 21:59:27 +0200

    started report, fixed bugs

--- Code/Main.py
+++ Code/Main.py
@@ -4,18 +4,22 @@ import torch.nn.functional as F
 from torch.nn import Linear, Embedding
 from torch_geometric.data import Data
 from torch_geometric.transforms import NormalizeFeatures
-from torch_geometric.datasets import SuiteSparseMatrixCollection
+from torch_geometric.datasets import SuiteSparseMatrixCollection, TUDataset
 from torch_geometric.loader import DataLoader
 from torch_geometric.nn import GCNConv
 from blossom import maxWeightMatching
 from LineGraphConverter import ToLineGraph
 from DataLoader import LoadData
+import pickle
 torch.manual_seed(123)
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 print(torch.version.cuda)
 
 
-dataset = SuiteSparseMatrixCollection('/Data', 'Newman', 'netscience',transform=NormalizeFeatures())
+dataset = SuiteSparseMatrixCollection('/Data', 'Newman', 'netscience', transform=NormalizeFeatures())
+print("dataset", len(dataset))
+proteins = TUDataset(root='/tmp/PROTEINS', name='PROTEINS')
+print("proteins", len(proteins))
 
 train_data = dataset[0]
 train_data.edge_weight=train_data.edge_attr.unsqueeze(1)
@@ -36,7 +40,15 @@ for i in range(len(train_data.edge_index[0])):
          train_data.edge_index[1][i].item(),
          train_data.edge_weight[i][0].item()))
 
-y = maxWeightMatching(blossominput)
+target_data = maxWeightMatching(blossominput)
+
+file_name = 'data/target_data.pkl'
+with open(file_name, 'wb') as file:
+    pickle.dump(target_data, file)
+    print(f'Object successfully saved to "{file_name}"')
+
+file = open(file_name, 'rb')
+y = pickle.load(file)
 
 print(f'matching: {y[:5]}')
 print(f'count: {len(y)}')
@@ -67,9 +79,12 @@ print(f'classes length: {len(train_data.y)}')
 class GCN(torch.nn.Module):
     def __init__(self):
         super().__init__()
-        self.conv1 = GCNConv(1, 100)
-        self.conv2 = GCNConv(100, 16)
-        self.conv3 = GCNConv(16, 2)
+        self.conv1 = GCNConv(1, 32)
+        self.conv2 = GCNConv(32, 32)
+        self.conv3 = GCNConv(32, 32)
+        self.conv4 = GCNConv(32, 32)
+        self.conv5 = GCNConv(32, 32)
+        self.conv6 = GCNConv(32, 2)
 
     def forward(self, data):
         x, edge_index = data.node_features, data.edge_index
@@ -79,15 +94,22 @@ class GCN(torch.nn.Module):
         x = self.conv2(x, edge_index)
         x = F.relu(x)
         x = self.conv3(x, edge_index)
+        x = F.relu(x)
+        x = self.conv4(x, edge_index)
+        x = F.relu(x)
+        x = self.conv5(x, edge_index)
+        x = F.relu(x)
+        x = self.conv6(x, edge_index)
         
         return F.log_softmax(x, dim=1)
     
 model = GCN().to(device)
 train_data = train_data.to(device)
-optimizer = torch.optim.Adam(model.parameters(), lr=0.01)#, weight_decay=0.01)
+optimizer = torch.optim.Adam(model.parameters(), lr=0.002)
 
 model.train()
-for epoch in range(1000):
+epochs = 1000
+for epoch in range(epochs):
     optimizer.zero_grad()
     out = model(train_data)
 
@@ -115,22 +137,40 @@ print("SORTED", sorted_pred)
 
 print("GREEDY PICK")
 picked_edges = {(-1,-1)}
+picked_nodes = {-1}
+weightSum = 0
 for i, sorted_i in enumerate(sorted_pred.indices):
-    #print("edge number = ", sorted_i)
     from_node = original_graph.edge_index[0][sorted_i].item()
     to_node = original_graph.edge_index[1][sorted_i].item()
-    if top.indices[sorted_i] == 1 and (from_node, to_node) not in picked_edges:
-        #print("PICKED edge number = ", sorted_i)
-        #print("PICKED edge = ", (from_node, to_node))
+    if top.indices[sorted_i] == 1 and from_node not in picked_nodes and to_node not in picked_nodes: #and (from_node, to_node) not in picked_edges:
+        weightSum += 2*original_graph.edge_weight[sorted_i]
         picked_edges.add((from_node, to_node))
+        picked_edges.add((to_node, from_node))
+        picked_nodes.add(from_node)
+        picked_nodes.add(to_node)
+        
+for i, sorted_i in enumerate(sorted_pred.indices):
+    from_node = original_graph.edge_index[0][sorted_i].item()
+    to_node = original_graph.edge_index[1][sorted_i].item()
+    if top.indices[sorted_i] == 0 and from_node not in picked_nodes and to_node not in picked_nodes:
+        weightSum += 2*original_graph.edge_weight[sorted_i]
+        picked_edges.add((from_node, to_node))
+        picked_edges.add((to_node, from_node))
+        picked_nodes.add(from_node)
+        picked_nodes.add(to_node)
+            
 
 print("ALL EDGES: ", original_graph.edge_index)
 print("GREEDY PICKED EDGES: ", picked_edges, "LEN =", len(picked_edges))
+
 true_edges_idx = (y == 1).nonzero(as_tuple=True)[0]
 true_edges = [(-1,-1)]
+
+weightMax = 0
 for idx in true_edges_idx:
     from_node = original_graph.edge_index[0][idx].item()
     to_node = original_graph.edge_index[1][idx].item()
+    weightMax += original_graph.edge_weight[idx]
     true_edges.append((to_node, from_node))        
 print("TRUE EDGES: ", true_edges_idx, "LEN =", len(true_edges))
 
@@ -147,6 +187,9 @@ for i in range(len(y)):
 
 print(f'correct: {correct}')  
 print(f'y: {len(train_data.y)}')  
+
+w = weightSum / weightMax
+print(f'Total Weight: {w.item():.4f}')
 
 acc = int(correct) / int(len(train_data.y))
 print(f'Total Accuracy: {acc:.4f}')
---
+++ Code/teststuff.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 28 21:58:58 2023
+
+@author: nikit
+"""
+
