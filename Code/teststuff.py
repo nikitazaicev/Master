f824c68872a610145297b869d67b7f40df08c298
nikitazaicev <45469997+nikitazaicev@users.noreply.github.com>
2023-08-29 13:32:07 +0200

    more data test

--- Code/teststuff.py
+++ Code/teststuff.py
@@ -1,7 +1,21 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Aug 28 21:58:58 2023
+import os.path as osp
+import torch
+import torch.nn.functional as F
+from torch_geometric.transforms import NormalizeFeatures
+from torch_geometric.datasets import SuiteSparseMatrixCollection, TUDataset
+from torch_geometric.loader import DataLoader
+from torch_geometric.nn import GCNConv
+from blossom import maxWeightMatching
+from LineGraphConverter import ToLineGraph
+from torch_geometric.loader import DataLoader
+import pickle
+torch.manual_seed(123)
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+print(torch.version.cuda)
 
-@author: nikit
-"""
+demo = SuiteSparseMatrixCollection('/Data', 'Newman', 'netscience', transform=NormalizeFeatures())
+print("dataset", len(demo))
+dataset = TUDataset(root='/tmp/PROTEINS', name='PROTEINS')
+print("proteins", len(dataset))
 
+loader = DataLoader(dataset, batch_size=32, shuffle=True)
